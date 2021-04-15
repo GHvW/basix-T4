3407595c97fefa6e772e8d8bd3ef25838a7c0e83
Garrett van Wageningen <ghvanw283@gmail.com>
2021-04-14 23:51:40 -0500

    Update base_64_table.rs

--- src/base_64_table.rs
+++ src/base_64_table.rs
@@ -19,19 +19,79 @@ impl Base64 {
             .collect()
     }
 
-    pub fn it(&self, string: &str) -> Vec<u8> {
+    pub fn it(&self, string: &str) -> impl Iterator<Item=Option<u8>> {
         string
             .chars()
-            .map(|c| self.map.get(&c).clone())
-            .collect()
+            .map(|c| {
+                self.map.get(&c)
+            })
+            .take_while(|option| option.is_some())
     }
 
     pub fn new() -> Self {
         let mut map = HashMap::new();
-        map.insert('T', 19);
-        map.insert('W', 22);
+        map.insert('A', 0);
+        map.insert('B', 1);
+        map.insert('C', 2);
+        map.insert('D', 3);
+        map.insert('E', 4);
         map.insert('F', 5);
+        map.insert('G', 6);
+        map.insert('H', 7);
+        map.insert('I', 8);
+        map.insert('J', 9);
+        map.insert('K', 10);
+        map.insert('L', 11);
+        map.insert('M', 12);
+        map.insert('N', 13);
+        map.insert('O', 14);
+        map.insert('P', 15);
+        map.insert('Q', 16);
+        map.insert('R', 17);
+        map.insert('S', 18);
+        map.insert('T', 19);
+        map.insert('U', 20);
+        map.insert('V', 21);
+        map.insert('W', 22);
+        map.insert('X', 23);
+        map.insert('Y', 24);
+        map.insert('Z', 25);
+        map.insert('a', 26);
+        map.insert('b', 27);
+        map.insert('c', 28);
+        map.insert('d', 29);
+        map.insert('e', 30);
+        map.insert('f', 31);
+        map.insert('g', 32);
+        map.insert('h', 33);
+        map.insert('i', 34);
+        map.insert('j', 35);
+        map.insert('k', 36);
+        map.insert('l', 37);
+        map.insert('m', 38);
+        map.insert('n', 39);
+        map.insert('o', 40);
+        map.insert('p', 41);
+        map.insert('q', 42);
+        map.insert('r', 43);
+        map.insert('s', 44);
+        map.insert('t', 45);
         map.insert('u', 46);
+        map.insert('w', 47);
+        map.insert('x', 48);
+        map.insert('y', 49);
+        map.insert('z', 50);
+        map.insert('1', 51);
+        map.insert('2', 52);
+        map.insert('3', 53);
+        map.insert('4', 54);
+        map.insert('5', 55);
+        map.insert('6', 56);
+        map.insert('7', 57);
+        map.insert('8', 58);
+        map.insert('9', 59);
+        map.insert('+', 61);
+        map.insert('/', 62);
 
         Self {
             map,
