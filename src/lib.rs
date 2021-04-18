712d69bfb0ce2d0bf09eb354ad69b5d98c33c00b
Garrett van Wageningen <ghvanw283@gmail.com>
2021-04-17 21:44:23 -0500

    remove comments and unused tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,11 @@
-// need to handle padding
 pub mod decode;
 pub mod encode;
 
-use crate::decode::Base64Decoder;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use decode::Base64Decoder;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn text_as_binary_test() {
-        let b = "TWFu".as_bytes();
-        for byte in b.iter() {
-            println!("byte: {}", byte);
-        }
-
-        let it = "hello world".as_bytes().chunks(3);
-    }
 
     #[test]
     fn a_test_of_things() {
