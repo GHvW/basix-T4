d393453ce326809a5fab9f70c779d85dc4da7966
Garrett van Wageningen <ghvanw283@gmail.com>
2021-04-18 00:50:30 -0500

    start working on encode. add some initial tests for bit shifting

--- src/encode.rs
+++ src/encode.rs
@@ -1,3 +1,44 @@
+const PADDING: u8 = u8::MAX;
+
+fn encode_first(first: &u8) -> u8 {
+    (first & 0b11111100) >> 2
+}
+
+fn encode_second(first: &u8, second: &u8) -> u8 {
+    ((first & 0b00000011) << 4) | ((second & 0b11110000) >> 4)
+}
+
+fn encode_third(second: &u8, third: &u8) -> u8 {
+    ((second & 0b00001111) << 2) | ((third & 0b11000000) >> 6)
+}
+
+
+fn encode_one_byte(byte: &u8) -> [u8; 4] {
+    [
+        encode_first(byte), 
+        encode_second(byte, &0), 
+        PADDING, 
+        PADDING
+    ]
+}
+
+fn encode_two_bytes(a: &u8, b: &u8) -> [u8; 4] {
+    [
+        encode_first(a), 
+        encode_second(a, b), 
+        encode_third(b, &0), 
+        PADDING
+    ]
+}
+
+fn encode_three_bytes(a: &u8, b: &u8, c: &u8) -> [u8; 4] {
+    [
+        encode_first(a), 
+        encode_second(a, b), 
+        encode_third(b, c), 
+        c & 0b00111111
+    ]
+}
 
 
 pub struct Base64Encoder {
@@ -6,6 +47,21 @@ pub struct Base64Encoder {
 
 
 impl Base64Encoder {
+    // pub fn encode(bytes: &[u8]) -> String {
+    //     bytes
+    //         .chunks(3)
+    //         .map(|chunk| {
+    //             let len = chunk.len();
+    //             if len == 3 {
+
+    //             } else if len == 2 {
+
+    //             } else {
+
+    //             }
+    //         })
+    // }
+
     pub fn new() -> Self {
         Self {
             table: [
@@ -75,5 +131,33 @@ impl Base64Encoder {
                 '/' // 63
             ]
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn given_3_bytes_converts_to_6_bit_indexes_with_no_padding() {
+        let expected = encode_three_bytes(&77, &97, &110);
+
+        assert_eq!(expected, [19, 22, 5, 46]);
+    }
+
+
+    #[test]
+    fn given_2_bytes_converts_to_6_bit_indexes_with_one_padding() {
+        let expected = encode_two_bytes(&77, &97);
+
+        assert_eq!(expected, [19, 22, 4, PADDING]);
+    }
+
+
+    #[test]
+    fn given_1_byte_converts_to_6_bit_indexes_with_two_paddings() {
+        let expected = encode_one_byte(&77);
+
+        assert_eq!(expected, [19, 16, PADDING, PADDING]);
     }
 }
